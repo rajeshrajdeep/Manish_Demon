44d6996eca505c9bf3d213ec7b161b4c25d4600e
Rajesh Kumar <rajeshrajdeep72@gmail.com>
2024-06-18 14:23:30 +0530

    added menu panel tabs

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import Bars from './ui/Bars'
-import Logo from './ui/Logo'
+"use client"
+import React, { useState } from 'react';
+import Bars from './ui/Bars';
+import Logo from './ui/Logo';
+import MenuPanel from './MenuPanel';
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <nav className='md:py-5 py-3 md:px-10 px-5 md:fixed sticky w-full z-50 top-0 md:bg-transparent bg-neutral-50 flex justify-between text-3xl md:text-neutral-50'>
       <div>
         <Logo />
       </div>
       <div>
-        <Bars />
+        <Bars isActive={isActive} setIsActive={setIsActive} />
       </div>
+      <MenuPanel isActive={isActive} />
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- components/ui/Bars.js
+++ components/ui/Bars.js
@@ -1,14 +1,11 @@
 "use client";
-import { faBarsStaggered, faPlus } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faBarsStaggered, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 
-export default function Bars() {
-  const [isActive, setIsActive] = React.useState(false);
-
+export default function Bars({ isActive, setIsActive }) {
   return (
-    <div className='z-50' onClick={() => setIsActive(!isActive)} // Toggle isActive on click
-    >
+    <div className='z-50' onClick={() => setIsActive(!isActive)}>
       {isActive ? (
         <FontAwesomeIcon className='transition-all hover:rotate-[135deg] rotate-45' icon={faPlus} />
       ) : (
